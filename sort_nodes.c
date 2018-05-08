7f36667ded471fd87a7c699bc0a038813990e716
enpengxu <enpengxu@gmail.com>
2018-05-08 16:15:32 -0400

    bug fix

--- sort_nodes.c
+++ sort_nodes.c
@@ -4,57 +4,35 @@
 #include <stdio.h>
 
 #define NODE_COLOR_WHITE 0
-#define NODE_COLOR_BLACK 1
+#define NODE_COLOR_GRAY  1
+#define NODE_COLOR_BLACK 2
 
-struct node {
-	int src;
-	int dst;
-
-	unsigned order;
-	unsigned color;
-	struct node * dep;
-	struct node * next;
-	struct node * prev;
-};
-
-struct node_list {
-	unsigned num;
-	struct node  head;
-};
-
-#define list_init(list)				   \
-	(list)->head.next = &(list)->head;	   \
+#define list_init(list)							\
+	(list)->head.next = &(list)->head;				\
 	(list)->head.prev = &(list)->head
 
 #define first_node(list)  (list)->head.next
 #define last_node(list)   (list)->head.prev
 
-#define for_each_node_next(node, list)				\
-	for (; (node) != &(list)->head; (node) = (node)->next)
+#define for_each_node(node, list, dir)						\
+	for (; (node) != &(list)->head; (node) = (node)->dir)
 
-#define for_each_node_prev(node, list)	\
-	for (; (node) != &(list)->head; (node) = (node)->prev)
+#define for_each_node_between(node, n0, n1, dir)			\
+	for (node=(n1)->prev; node != (n0); node = node->dir)
 
-#define for_each_node_between_prev(node, n0, n1)	\
-	for (node=(n1)->prev; node != (n0); node = node->prev)
+struct node {
+	int           src;
+	int           dst;
+	unsigned      order;
+	unsigned      color;
+	struct node * next;
+	struct node * prev;
+};
 
-#define remove_node(node)							\
-	(node)->next->prev = (node)->prev;				\
-	(node)->prev->next = (node)->next
-
-/* insert node before n*/
-#define insert_node_before(node, n)	\
-	(node)->next = (n);				\
-	(node)->prev = (n)->prev;			\
-	(n)->prev->next = (node);			\
-	(n)->prev = (node)
-
-/* insert node after n*/
-#define insert_node_after(node, n)	\
-	(node)->next = (n)->next;			\
-	(node)->prev = (n);				\
-	(n)->next->prev = (node);			\
-	(n)->next = (node)
+struct node_list {
+	unsigned     num;
+	struct node  head;
+};
 
 static void
 node_list_init(struct node_list * list)
@@ -63,21 +41,21 @@ node_list_init(struct node_list * list)
 	struct node * node;
 
 	node = first_node(list);
-	for_each_node_next(node, list) {
+	for_each_node(node, list, next) {
 		node->order = order ++;
-		node->dep  = NULL;
 	}
+
 	/* init dep */
-	node = last_node(list);
-	for_each_node_prev(node, list) {
-		struct node * prev = node->prev;
-		for_each_node_prev(prev, list) {
-			if (prev->dst == node->src) {
-				node->dep = prev;
-				break;
-			}
-		}
-	}
+	/* node = last_node(list); */
+	/* for_each_node(node, list, prev) { */
+	/* 	struct node * prev = node->prev; */
+	/* 	for_each_node(prev, list, prev) { */
+	/* 		if (prev->dst == node->src) { */
+	/* 			node->dep = prev; */
+	/* 			break; */
+	/* 		} */
+	/* 	} */
+	/* } */
 }
 
 static inline struct node *
@@ -86,7 +64,7 @@ node_find_first_dep(struct node_list * list,
 			  struct node * start)
 {
 	struct node * node = start;
-	for_each_node_prev(node, list) {
+	for_each_node(node, list, prev) {
 		if (node->dst == dst)
 			return node;
 	}
@@ -100,13 +78,14 @@ node_is_depend_on(struct node_list * list,
 				 struct node * node,
 				 struct node * target)
 {
-	struct node * dep_src;
-	struct node * dep_dst;
 	if (node->order < target->order)
 		return 0;
 	if (node->dst == target->dst ||
 		node->src == target->dst)
 		return 1;
+
+	struct node * dep_src;
+	struct node * dep_dst;
 	dep_src  = node_find_first_dep(list, node->src, node->prev);
 	dep_dst  = node_find_first_dep(list, node->dst, node->prev);
 
@@ -125,47 +104,11 @@ node_is_mergable(struct node_list * list,
 				 struct node * n1)
 {
 	struct node * node, *dep;
-	for_each_node_between_prev(node, n0, n1) {
+	for_each_node_between(node, n0, n1, prev) {
 		if (node_is_depend_on(list, node, n0))
 			return 0;
 	}
 	return 1;
-}
-/**
- * merge n0 & n1 if possible
- * n0->dst == n1->dst
- */
-static int
-node_merge(struct node_list * list,
-		   struct node * n0,
-		   struct node * n1)
-{
-	int i, reorder = 0;
-	struct node * node, * bottom = n0;
-
-	assert(n0 && n1 && n0->dst == n1->dst);
-
-	node = n1->dep;
-	while(node && node->order < n0->order) {
-		assert(node->order < n0->order);
-		remove_node(node);
-		insert_node_before(node, bottom);
-		bottom = node;
-		node = node->dep;
-		reorder = 1;
-	}
-	remove_node(n1);
-	insert_node_after(n1, n0);
-	if (1 || reorder) {
-		/* re-assign orders */
-		/* TODO. no need to go through all the nodes */
-		int order = 1;
-		node = first_node(list);
-		for_each_node_next(node, list) {
-			node->order = order ++;
-		}
-	}
-	return 0;
 }
 
 static void
@@ -176,16 +119,16 @@ node_list_sort(struct node_list * list, struct node **ptr_list)
 
 	unsigned num = list->num - 1;
 	n1 = last_node(list);
-	for_each_node_prev(n1, list) {
+	for_each_node(n1, list, prev) {
 		if (n1->color == NODE_COLOR_BLACK)
 			continue;
 
 		ptr_list[num--] = n1;
 		n0 = n1;
 		tmp = n1;
+
 		while(n0 = node_find_first_dep(list, n0->dst, n0->prev)) {
 			if (node_is_mergable(list, n0, tmp)) {
-				//node_merge(list, n0, n1);
 				n0->color = NODE_COLOR_BLACK;
 				ptr_list[num--] = n0;
 				tmp = n0;
@@ -232,7 +175,7 @@ node_list_dump(struct node_list * list, const char * str)
 	struct node * node = first_node(list);
 	fprintf(stderr, "%s \n", str);
 
-	for_each_node_next(node, list) {
+	for_each_node(node, list, next) {
 		fprintf(stderr, "node[%2d] : src/dst { %3d / %3d } \n",
 				i++, node->src, node->dst);
 	}
@@ -250,19 +193,19 @@ node_ptr_list_dump(struct node ** ptr_list, unsigned num, const char * str)
 	fprintf(stderr, "\n");
 }
 
-static struct dst_src test1[]=  {
+static struct dst_src test1[]= {
 	{ .src = 101, .dst = 1 },
 	{ .src = 102, .dst = 2 },
 	{ .src = 104, .dst = 1 }
 };
 
-static struct dst_src test2[]=  {
+static struct dst_src test2[]= {
 	{ .src = 2, .dst = 1 },
 	{ .src = 1, .dst = 2 },
 	{ .src = 2, .dst = 1 }
 };
 
-static struct dst_src test3[]=  {
+static struct dst_src test3[]= {
 	{ .src = 100, .dst = 4 },
 	{ .src = 101, .dst = 1 },
 	{ .src =   4, .dst = 3 },
@@ -274,15 +217,24 @@ static struct dst_src test3[]=  {
 static struct dst_src test4[]=  {
 	{ .src = 100, .dst = 4 },
 	{ .src = 101, .dst = 1 },
-	{ .src =   1, .dst = 2 }, /* it should stop merging */
+	{ .src =   1, .dst = 2 }, /* it should stop merging (101/1)-(2/1)*/
 	{ .src =   4, .dst = 3 },
 	{ .src =   3, .dst = 2 },
 	{ .src =   2, .dst = 1 }
 };
 
+static struct dst_src test5[]=  {
+	{ .src = 100, .dst = 4 },
+	{ .src = 101, .dst = 1 },
+	{ .src =   1, .dst = 2 },
+	{ .src = 104, .dst = 3 },
+	{ .src =   3, .dst = 2 },
+	{ .src =   2, .dst = 4 }
+};
+
 
 static int
-do_test (struct dst_src * test, int num)
+run_test (struct dst_src * test, int num)
 {
 	struct node_list list;
 	struct node ** ptr_list;
@@ -297,9 +249,15 @@ do_test (struct dst_src * test, int num)
 
 int main(int argc, char **argv)
 {
-	do_test(test1, sizeof(test1)/sizeof(test1[0]));
-	do_test(test2, sizeof(test2)/sizeof(test2[0]));
-	do_test(test3, sizeof(test3)/sizeof(test3[0]));
-	do_test(test4, sizeof(test4)/sizeof(test4[0]));
+#define test(n) \
+	fprintf(stderr, "testing "#n "\n");\
+	run_test(test##n, sizeof(test##n)/sizeof(test##n[0]))
+
+	test(1);
+	test(2);
+	test(3);
+	test(4);
+	test(5);
+
 	return 0;
 }
